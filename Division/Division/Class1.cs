cf6d3a3d310359ded2035d39b56c447dfd8d4668
Cristian06Meneses <cristianmeneses979@gmail.com>
2023-03-28 20:41:00 -0500

    Programa terminado :D

--- Division/Division/Class1.cs
+++ Division/Division/Class1.cs
@@ -2,11 +2,15 @@
 {
     public class Class1
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
         public static float division(int x, int z)
         {
             return (x / z);
         }
-
-
     }
 }
--- Multiplicacion/Multiplicacion/Class1.cs
+++ Multiplicacion/Multiplicacion/Class1.cs
@@ -2,10 +2,12 @@
 {
     public class Class1
     {
-        // Metodo multiplicacion
-        //Toma el primer valor "n1" enviado como "num1"
-        //toma el segundo valor "n2" enviado como "num2"
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
         public static int multiplicacion(int n1, int n2)
         {
             return (n1 * n2);
